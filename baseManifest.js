93f305412fb8f0a8f328e19a34bbe258a1cdb6b9
temrix <benjamin.doppler@gmail.com>
2020-04-08 20:00:24 +0200

    Migrate (from yo) to webpack-based framework.

---
+++ baseManifest.js
@@ -0,0 +1,13 @@
+const pkg = require('./package.json')
+
+module.exports = {
+  manifest_version: 2,
+  name: 'Ektoplazm Noflash',
+  description: pkg.description,
+  version: pkg.version,
+
+  content_scripts: [{
+    matches: ['*://ektoplazm.com/*'],
+    js: [`${pkg.name}.user.js`]
+  }]
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const WebpackUserscript = require('webpack-userscript')
+const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin')
+const baseManifest = require('./baseManifest.js')
+const pkg = require('./package.json')
+const dev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `${pkg.name}.user.js`
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new WebpackUserscript({
+      headers: {
+        name: baseManifest.name,
+        version: dev ? `[version]-build.[buildNo]` : `[version]`,
+        match: baseManifest.content_scripts.matches,
+        license: pkg.license,
+        namespace: pkg.author
+      }
+    }),
+    new WebpackExtensionManifestPlugin({
+      config: {
+        base: baseManifest,
+        extend: {
+          version: pkg.version
+        }
+      }
+    })
+  ]
+}
